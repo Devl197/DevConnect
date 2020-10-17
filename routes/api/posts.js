31eddd75f3a20782752be121db9e21652f189e7d
Aleksandar Mladenovic <ml_aleksandar@hotmail.com>
2020-10-17 13:50:08 +0200

    Added create a post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,8 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+const User = require('../../models/User');
+const Profile = require('../../models/Profile');
+const Post = require('../../models/Post');
 
-router.get('/', (req, res) => {
-  res.send('Posts route');
-});
+// Create a post
+router.post(
+  '/',
+  [auth, [check('text', 'Text is required').notEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
