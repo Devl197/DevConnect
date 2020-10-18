ce2f59b159f820f8b54f6f33aafaa4ee22fc85c2
Aleksandar Mladenovic <ml_aleksandar@hotmail.com>
2020-10-18 12:45:24 +0200

    Added Login and Register components and some changes to the App, Landing and Navbar components

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,21 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const App = () => (
-  <>
+  <Router>
     <Navbar />
-    <Landing />
-  </>
+    <Route exact path='/' component={Landing} />
+    <section className='container'>
+      <Switch>
+        <Route exact path='/register' component={Register} />
+        <Route exact path='/login' component={Login} />
+      </Switch>
+    </section>
+  </Router>
 );
 
 export default App;
