9745686907fecee2a53d8b14d5cd4074c5759620
Aleksandar Mladenovic <ml_aleksandar@hotmail.com>
2020-10-18 12:27:44 +0200

    Added create-react-app

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+
+const App = () => (
+  <>
+    <Navbar />
+    <Landing />
+  </>
+);
+
+export default App;
