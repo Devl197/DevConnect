b7441228f8a1061c307074cfd118e853f177cff5
Aleksandar Mladenovic <ml_aleksandar@hotmail.com>
2020-10-20 14:25:46 +0200

    Added AddEducation and AddExperience components

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,8 @@ import { Provider } from 'react-redux';
 import store from './store';
 import setAuthToken from './utils/setAuthToken';
 import { loadUser } from './actions/auth';
+import AddExperience from './components/profile-forms/AddExperience';
+import AddEducation from './components/profile-forms/AddEducation';
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -41,6 +43,16 @@ const App = () => {
               component={CreateProfile}
             />
             <PrivateRoute exact path='/edit-profile' component={EditProfile} />
+            <PrivateRoute
+              exact
+              path='/add-experience'
+              component={AddExperience}
+            />
+            <PrivateRoute
+              exact
+              path='/add-education'
+              component={AddEducation}
+            />
           </Switch>
         </section>
       </Router>
