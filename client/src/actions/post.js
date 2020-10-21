26b96021933bea6a175186075d84ae6d972e5c00
Aleksandar Mladenovic <ml_aleksandar@hotmail.com>
2020-10-21 22:57:17 +0200

    Added CommentForm and CommentItem components

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -7,6 +7,8 @@ import {
   UPDATE_LIKES,
   ADD_POST,
   GET_POST,
+  ADD_COMMENT,
+  REMOVE_COMMENT,
 } from './types';
 
 export const getPosts = () => async dispatch => {
@@ -127,6 +129,60 @@ export const getPost = id => async dispatch => {
       payload: res.data,
     });
   } catch (error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
+    });
+  }
+};
+
+export const addComment = (postId, formData) => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  try {
+    const res = await axios.post(
+      `/api/posts/comment/${postId}`,
+      formData,
+      config
+    );
+
+    dispatch({
+      type: ADD_COMMENT,
+      payload: res.data,
+    });
+
+    dispatch(setAlert('Comment Added', 'success'));
+  } catch (error) {
+    console.log(error);
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
+    });
+  }
+};
+
+export const deleteComment = (postId, commentId) => async dispatch => {
+  try {
+    await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+
+    dispatch({
+      type: REMOVE_COMMENT,
+      payload: commentId,
+    });
+
+    dispatch(setAlert('Comment Removed', 'success'));
+  } catch (error) {
+    console.log(error);
     dispatch({
       type: POST_ERROR,
       payload: {
--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -18,7 +18,11 @@ const ProfileGithub = ({ username, getGithubRepos, repos }) => {
           <div key={repo.id} className='repo bg-white p-1 my-1'>
             <div>
               <h4>
-                <a href={repo.html_url} target='_blank' rel='noopener noreferr'>
+                <a
+                  href={repo.html_url}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
                   {repo.name}
                 </a>
               </h4>
